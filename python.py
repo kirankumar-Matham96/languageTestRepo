ebc06b3940ef8a4625735ae3f5449fb1efff18be
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-11-26 12:16:21 +0530

    update: added extra code for python

--- python.py
+++ python.py
@@ -9,4 +9,18 @@ def is_prime(number):
             return False
     return True
 
+def print_fibonacci(n):
+    num1 = 0
+    num2 = 1
+    next_number = num2  
+    count = 1
+
+    while count <= n:
+        print(next_number, end=" ")
+        count += 1
+        num1, num2 = num2, next_number
+        next_number = num1 + num2
+    print()
+
 print(is_prime(num))
+print(print_fibonacci(50))
