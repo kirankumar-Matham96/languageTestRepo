b453e14cfce9aafbc3b7073bc7a63789437d0a2a
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-11-26 12:13:06 +0530

    feat: added extra code for java and python files

--- python.py
+++ python.py
@@ -1,4 +1,12 @@
-x = 10
-y = 20
-z = x + y
-print(z)
+num = 12
+
+def is_prime(number):
+    if number == 0 or number == 1:
+        return False
+    
+    for i in range(2, int(number/2)):
+        if number%i == 0:
+            return False
+    return True
+
+print(is_prime(num))
